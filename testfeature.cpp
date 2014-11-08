1bf88ca77e346a2eb60923691ccbd877edff9a38
Kaiyu Zheng <kaiyutony@gmail.com>
2014-11-08 00:59:18 -0800

    Added a test

---
+++ testfeature.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  if (1 + 1 == 2)
+    {
+      std::cout << "So Smart!" << std::endl;
+    }
+  else
+    {
+      std::cout << "You should not use computer" << std::endl;
+    }
+}
